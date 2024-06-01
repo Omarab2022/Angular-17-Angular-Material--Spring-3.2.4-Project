4a2652fda819a568a2f624d63861614b3e95f81a
OMAR ABARRA <abarraomar@gmail.com>
2024-06-01 18:32:49 +0100

    1 jun

--- backend/src/main/java/com/backend/demo/Controllers/StudentController.java
+++ backend/src/main/java/com/backend/demo/Controllers/StudentController.java
@@ -3,14 +3,21 @@ package com.backend.demo.Controllers;
 
 import com.backend.demo.Entity.Payment;
 import com.backend.demo.Entity.Student;
+import com.backend.demo.Enums.PaymentType;
 import com.backend.demo.Repository.PaymentRepo;
 import com.backend.demo.Repository.StudentRepo;
 import lombok.Getter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class StudentController {
@@ -47,5 +54,37 @@ public class StudentController {
 	@GetMapping("/students/{id}")
 	public Student findById(@PathVariable String id){
 		return studentRepo.findById(id).get();
+	}
+
+	@PostMapping(value = "/payments",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public Payment savepayment(@RequestParam MultipartFile file ,
+	                           LocalDate date ,
+	                           double amount ,
+	                           PaymentType type ,
+	                           String studentcode) throws IOException {
+
+
+		Path path = Paths.get(System.getProperty("user.home"),"students-app-files","payments");
+
+		if (!Files.exists(path)){
+            Files.createDirectories(path);
+		}
+		String paymentId = UUID.randomUUID().toString();
+		Path filePath = Paths.get(System.getProperty("user.home"),"students-app-files","payments",paymentId+".pdf");
+
+		Files.copy(file.getInputStream(),filePath);
+
+		
+		Student student = studentRepo.findByCode(studentcode);
+
+		Payment payment = Payment.builder()
+				.date(date)
+				.amount(amount)
+				.type(type)
+				.file(file.getOriginalFilename())
+				.student(student)
+				.build();
+
+		return payment;
 	}
  }
